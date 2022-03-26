ca193c795d37f3ec6828e41a0788f4805d9f108c
Foysal Ahamed Badsha <you@example.com>
2022-03-26 14:21:05 +0600

    forth commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+    let total = 0;
+    let Image;
+    for (const product of cart) {
+        total = total + product.price;
+        Image = product.img;
+    }
+    const tax = parseFloat((total * 0.05).toFixed(2));
+    const finalTotalPrice = total + tax;
     return (
-        <div>
+        <div className='fixed-cart'>
             <h3>Show-order</h3>
-            {<p>Selected Item: {cart.length}</p>}
+            <p>Image : {Image} </p>
+            <p>Selected Item: {cart.length}</p>
+            <p>Total price : $ {total}</p>
+            <p>Tax : $ {tax}</p>
+            <p>Final price : $ {finalTotalPrice.toFixed(2)}</p>
+
         </div>
     );
 };
